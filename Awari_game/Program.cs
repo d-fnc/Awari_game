e541b707580d18606e546b02459c33e9385554c9
Ferenc Dobó <ferenc.db@gmail.com>
2018-11-07 16:53:16 +0100

    init + cointoss start

--- Awari_game/Program.cs
+++ Awari_game/Program.cs
@@ -9,6 +9,17 @@ namespace Awari_game
             Console.WriteLine("Szia, üdvözöllek az Awari játékban! A kezdéshez add meg a neved(opcionális)!");
             string name = Console.ReadLine();
             Player human = name == ""?new Player():new Player(name);
+            Player cpu = new Player("HAL9000");
+
+            bool next = CoinToss();
+
+        }
+
+        static bool CoinToss()
+        {
+            bool start = false;
+
+            return start;
         }
     }
 }
